1b9976ade87be2de303b2af71968929175e8614e
hichd <hicham_daou@hotmail.com>
2020-04-18 16:02:25 -0400

    added dropdownmenu dropdownitem and managed icon positioning
    git push

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as PlusIcon } from './icons/plus.svg';
-// import { ReactComponent as CaretIcon } from './icons/caret.svg';
-// import { ReactComponent as CogIcon } from './icons/cog.svg';
-// import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
+import { ReactComponent as CogIcon } from './icons/cog.svg';
+import { ReactComponent as ChevronIcon } from './icons/chevron.svg';
 // import { ReactComponent as ArrowIcon } from './icons/arrow.svg';
 // import { ReactComponent as BoltIcon } from './icons/bolt.svg';
 
@@ -15,6 +15,11 @@ function App() {
       <NavItem icon={<PlusIcon />} />
       <NavItem icon={<BellIcon />} />
       <NavItem icon={<MessengerIcon />} />
+      <NavItem icon={<CaretIcon />} >
+
+        <DropdownMenu />
+
+      </NavItem>
     </Navbar>
   );
 }
@@ -28,13 +33,46 @@ function Navbar(prpos) {
 }
 
 function NavItem(props) {
+
+  const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
         {props.icon}
       </a>
+
+      {open && props.children}
     </li>
   );
 }
+
+function DropdownMenu() {
+
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+
+        {props.children}
+
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>My profile</DropdownItem>
+      <DropdownItem
+        leftIcon={<CogIcon />}>
+      </DropdownItem>
+    </div>
+  );
+
+}
+
+
+
 
 export default App;
